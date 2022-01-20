8d7e59b729c18f294d3d3df387a10760c5718b2b
Thomas Ngo <thomasngo818@gmail.com>
2022-01-20 04:01:46 +0000

    pair programming challenge completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    // look for word horizonatally
 
-module.exports = wordSearch
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+    // transpose the matrix
+    let output = [];
+    const rowsLetters = letters.length;
+    const colsLetters = letters[0].length;
+
+    for (let i = 0; i < rowsLetters; i++) {
+        output[i] = [];
+        for (let j = 0; j < colsLetters; j++) {
+            output[i][j] = letters[j][i];
+        }
+    }
+    // look horizontally again
+    const verticalJoin = output.map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
+};
+
+module.exports = wordSearch;
+
